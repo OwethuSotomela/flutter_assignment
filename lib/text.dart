3cdd10c350beb4cbb5f59843fd161f79f745bec8
OwethuSotomela <owethusotomela@gmail.com>
2022-01-11 14:08:32 +0200

    Changing text

--- lib/text.dart
+++ lib/text.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import './textControl.dart';
 
 class TextWidget extends StatefulWidget {
+  final String day;
+
+  TextWidget(this.day);
+  
   @override
   State<TextWidget> createState() => _TextWidgetState();
 }
 
 class _TextWidgetState extends State<TextWidget> {
   build(context) {
-    return Text('Manifestation is real');
+    return Container(
+      child: Center(
+        child: Text(widget.day),
+      ),
+    );
   }
 }
--- lib/textControl.dart
+++ lib/textControl.dart
@@ -2,31 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_course/text.dart';
 
 class TextControl extends StatefulWidget {
-  final String text = 'New Random text';
-
   @override
   TextControlState createState() => new TextControlState();
 }
 
 class TextControlState extends State<TextControl> {
-  // String get facts;
-  //  =  [
-  //   'Affirmations work',
-  //   'Manifestation is real',
-  //   'Happiness is a choice!'
-  // ];
+  var days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+
+  var weekdays = 0;
+
+  void changeText() {
+    setState(() {
+      weekdays = weekdays + 1;
+    });
+    print(weekdays);
+  }
 
   build(context) {
     return Column(
       children: [
-        Text('Inspirational quotes!'),
+        Text('Weekdays!'),
         ElevatedButton(
           child: Text('Change Text 1'),
-          onPressed: () {
-            setState(() {});
-          },
+          onPressed: changeText,
         ),
-        TextWidget()
+        TextWidget(days[weekdays])
       ],
     );
   }
